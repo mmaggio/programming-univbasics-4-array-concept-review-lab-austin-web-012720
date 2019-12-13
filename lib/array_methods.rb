dd89c6c3f18a4f6cf0d8c810973cc107a9d3ff0e
Francis Michael Maggio <bluebassist2002@yahoo.com>
2019-12-13 23:48:26 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,14 +1,14 @@
 def find_element_index(array, value_to_find)
  counter = 0
-nil_length = array.length + 1
+ nil_length = array.length + 1
   while counter < nil_length do
     break if array[counter] == value_to_find
     counter = counter + 1
    end
   if counter == nil_length
     return nil
-else
-return counter
+  else
+    return counter
   end
 end
 
